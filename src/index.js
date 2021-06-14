b18e0934e6f4037075705770c751233a1c9ba242
Paco <paco.fernandez@masvoz.es>
2021-06-14 13:40:05 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,67 @@
+const fetch = require("node-fetch");
+const { ApolloServer, gql } = require("apollo-server");
+
+
+// Type definitions define the "shape" of your data and specify
+// which ways the data can be fetched from the GraphQL server.
+const typeDefs = gql`
+  # Comments in GraphQL are defined with the hash (#) symbol.
+
+  enum CharacterStatus {
+    Alive
+    Dead
+    unknown
+  }
+  type Character {
+    name: String
+    id: ID
+    status: CharacterStatus
+    episodes: [String]
+
+  }
+
+  # The "Query" type is the root of all GraphQL queries.
+  # (A "Mutation" type will be covered later on.)
+  type Query {
+    characters: [Character]
+    character(id: ID): Character
+  }
+`;
+
+// Resolvers define the technique for fetching the types in the
+// schema.  We'll retrieve books from the "books" array above.
+const resolvers = {
+  Query: {
+    characters: () => fetchCharacters(),
+    character: (parent, args) => {
+      const { id } = args
+      return fetchCharacterById( id )
+    }
+  }
+};
+
+// In the most basic sense, the ApolloServer can be started
+// by passing type definitions (typeDefs) and the resolvers
+// responsible for fetching the data for those types.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// This `listen` method launches a web-server.  Existing apps
+// can utilize middleware options, which we'll discuss later.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
+
+
+function fetchCharacters() {
+  // More info about the fetch function? https://github.com/bitinn/node-fetch#json
+  return fetch("https://rickandmortyapi.com/api/character/")
+    .then(res => res.json())
+    .then(json => json.results);
+}
+
+function fetchCharacterById(id) {
+  // More info about the fetch function? https://github.com/bitinn/node-fetch#json
+  return fetch("https://rickandmortyapi.com/api/character/" + id)
+    .then(res => res.json())
+    .then(json => json);
+}
